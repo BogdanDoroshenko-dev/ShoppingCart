9f52670b3801229b91dafc562a51cccb832f8a7a
BogdanDoroshenko-dev <bogdandoroshenko777@gmail.com>
2026-06-24 13:40:41 +0300

    Added context folder with the CartContext file which handles all the cart logic and context for access to cart in other components.Wrapped the app in context in main.jsx

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,51 @@
+import { useState,useContext, createContext } from "react";
+
+const CartContext=createContext();
+
+export  function CartProvider({children}){
+const [cart,setCart]=useState([]);
+
+const addToCart=(product)=>{
+    setCart(prev=>{
+        const exists=prev.find(item=>item.id===product.id)
+
+        if(exists){
+            return prev.map(item=>
+                item.id===product.id
+                ?{...item,quantity:item.quantity+product.quantity}
+                :
+                item
+
+                )
+            }
+           return[...prev,product]
+        })
+
+    }
+
+
+const removeFromCart=(id)=>{
+    setCart(prev=>prev.filter(item=>item.id!==id))
+}
+
+
+const updateQuantity=(id,quantity)=>{
+if(quantity<1){
+    removeFromCart(id);
+    return;
+}
+    setCart(prev=>prev.map(item=>item.id===id?{...item,quantity}:item
+
+        )
+    )
+}
+return(
+    <CartContext.Provider value={cart,addToCart,removeFromCart,updateQuantity}>
+        {children}
+    </CartContext.Provider>
+)
+    
+}
+export function useCart() {
+    return useContext(CartContext)
+}
