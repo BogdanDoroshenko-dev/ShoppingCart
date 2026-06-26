9c665e36dc3278582a7de7d59cf5f40dd9e1af2a
BogdanDoroshenko-dev <bogdandoroshenko777@gmail.com>
2026-06-26 15:53:11 +0300

    Fixed the import bugs for the app to properly load, also added fetch functionality to Catalog file

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react"
+import {useCart} from  '../context/CartContext.jsx'
+
 const Catalog=()=>{
+  const [products, setProducts]=useState([]);
+
+  useEffect(()=>{
+    const fetchProducts=async()=>{
+     const responce= await fetch('https://fakestoreapi.com/products')
+     const data = await responce.json();
+     setProducts(data);
+    }
+    fetchProducts()
+  }
+
+
+,[])
 return(<>
-<span>Catalog</span>
+{products.map(product=>(
+<div key={product.id}>
+<img src={product.image} alt= {product.title} />
+<h3>{product.title}</h3>
+<p>{product.price}</p>
+<button>Add to cart</button>
+</div>
+))
+}
   </>)
 
 }
