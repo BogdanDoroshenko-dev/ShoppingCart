6aab71c34e512787042b78ca50e3402ce3bcc33e
BogdanDoroshenko-dev <bogdandoroshenko777@gmail.com>
2026-07-04 14:08:17 +0300

    Added the quantity state and functionality to change it with button near the products

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -3,6 +3,8 @@ import {useCart} from  '../context/CartContext.jsx'
 
 const Catalog=()=>{
   const [products, setProducts]=useState([]);
+  const [quantities, setQuantities]=useState({});
+
 
   useEffect(()=>{
     const fetchProducts=async()=>{
@@ -15,12 +17,36 @@ const Catalog=()=>{
 
 
 ,[])
+
+  const increment = (id) => {
+    setQuantities(prev=>({...prev,[id]:(prev[id]||1) + 1}))
+  }
+
+  const decrement = (id) => {
+    setQuantities(prev => ({ ...prev, [id]: Math.max(1, (prev[id] || 1) - 1) }))
+  }
+
 return(<>
 {products.map(product=>(
 <div key={product.id}>
 <img src={product.image} alt= {product.title} />
 <h3>{product.title}</h3>
 <p>{product.price}</p>
+
+    <div>
+        <button onClick={() => decrement(product.id)}>-</button>
+        <input 
+            type="number"
+            value={quantities[product.id] || 1}
+            onChange={e => setQuantities(prev => ({ 
+                ...prev, 
+                [product.id]: Math.max(1, parseInt(e.target.value) || 1) 
+            }))}
+        />
+        <button onClick={() => increment(product.id)}>+</button>
+    </div>
+
+
 <button>Add to cart</button>
 </div>
 ))
